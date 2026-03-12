17d00cf6a3f6367b384abeb3e751e90d0e7d767d
HERBERT MOSES <110626716+herbertmoses@users.noreply.github.com>
2026-03-12 20:01:08 +0530

    Refactor import statements in auth.py
    Updated import statements to remove relative paths.

--- routers/auth.py
+++ routers/auth.py
@@ -2,10 +2,10 @@ from datetime import datetime, timedelta
 
 from fastapi import FastAPI, APIRouter, Depends, HTTPException, Request
 from pydantic import BaseModel
-from ..models import Users
+from models import Users
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
-from ..database import SessionLocal
+from database import SessionLocal
 from typing import Annotated
 from starlette import status
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer

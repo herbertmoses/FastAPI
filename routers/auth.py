e9f7736e16a1643233c55f6dd527a953f92ef2b8
HERBERT MOSES <110626716+herbertmoses@users.noreply.github.com>
2026-03-14 15:31:24 +0530

    Log username and password in login function
    Added print statements to log received username and password during login.

--- routers/auth.py
+++ routers/auth.py
@@ -172,6 +172,8 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
 @router.post("/token", response_model=Token)
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
     user = authenticate_user(form_data.username, form_data.password, db)
+    print("Received username:", form_data.username)
+    print("Received password:", form_data.password)
     if not user:                  
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user.")
     token = create_access_token(user.username, user.id, user.role, timedelta(minutes=20))
